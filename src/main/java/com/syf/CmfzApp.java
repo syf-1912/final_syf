04fbd3e137071cf16d04f9136337b63d5052f641
syf <214372050@qq.com>
2018-08-28 20:51:04 +0800

    搭建开发环境

---
+++ src/main/java/com/syf/CmfzApp.java
@@ -0,0 +1,11 @@
+package com.syf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmfzApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CmfzApp.class,args);
+    }
+}
