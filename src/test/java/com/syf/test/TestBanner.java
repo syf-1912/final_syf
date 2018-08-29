7653ed953594aaa922453ace0a3bb38e6aa14c36
syf <214372050@qq.com>
2018-08-29 22:20:22 +0800

    为选项卡添加增删改查功能

--- src/test/java/com/syf/test/TestBanner.java
+++ src/test/java/com/syf/test/TestBanner.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
 import java.util.List;
 
 @RunWith(value = SpringRunner.class)
@@ -23,5 +24,12 @@ public class TestBanner {
         for (Banner banner : banners) {
             System.out.println(banner);
         }
+    }
+
+    @Test
+    public void test2() {
+        Banner banner = new Banner(3, "嘻嘻嘻", "/img/captcha.jpg", "测试", "N", new Date());
+
+        bannerDao.update(banner);
     }
 }
